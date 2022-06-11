c62d9272d3f6a1e68c15c34b309c508a3cb86817
Charlie Ontiveros <dev@charlietheprogrammer.com>
2022-06-11 13:13:36 -0700

    Init commit.

---
+++ app/Queries/ProductSimpleQuery.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Queries;
+
+use App\Models\Product;
+
+class ProductSimpleQuery
+{
+    private string $productId;
+
+    public function __construct(string $productId)
+    {
+        $this->productId = $productId;
+    }
+
+    //
+    public function getData(): array
+    {
+        $product = Product::findOrFail($this->productId);
+
+        return $product->toArray();
+    }
+}
